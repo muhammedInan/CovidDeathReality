fa44ba331003b6bd990fde855d3c8c0fb6a5c7e2
muhammedInan <muhammed-inan@outlook.com>
2021-03-05 22:04:17 +0000

    parse file all death deated

--- Covid/CovidDeath.php
+++ Covid/CovidDeath.php
@@ -2,12 +2,18 @@
 
 $monfichier = fopen('stat/2020.txt', 'r+');
 
+//$ligne = fgets($monfichier);
+ //echo substr($ligne, 154, 8);
+ 
 
- //155-162
-
-$ligne = fgets($monfichier);
- echo substr($ligne, 154, 8);
-
+ if ($monfichier) {
+    while (($ligne = fgets($monfichier, 154)) !== false) {
+        echo $$ligne;
+    }
+    if (!feof($ligne)) {
+        echo "Erreur: fgets() a échoué\n";
+    }
+ }
 
 fclose($monfichier);
 ?>
